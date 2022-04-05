37e519746c91e9964b967e0c95768c76288d9c8c
Mehdihussainmalla <mehdihussainmalla@gmail.com>
2022-04-05 22:53:14 +0530

    update regex

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import WrapperContainer from '../../Components/WrapperContainer';
 import {
   View,
@@ -8,14 +8,14 @@ import {
   ImageBackground,
   Button,
 } from 'react-native';
-import {styles} from './styles';
+import { styles } from './styles';
 import imagePath from '../../constants/imagePath';
 import navigationStrings from '../../navigation/navigationStrings';
-import {scale, verticalScale, moderateScale} from 'react-native-size-matters';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-export default function Login({navigation}) {
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+export default function Login({ navigation }) {
   const emailRegex = /^[\w-\.\_\$]{2,}@([\w]{3,5}\.)[\w]{2,4}$/;
-  const passRegex =/^[0-9]{4,8} $/.test(password);
+  const passRegex = /^[0-9]{4,8} $/;
 
   const [email, setemail] = useState('');
   const [emailerror, setemailError] = useState(false);
@@ -23,34 +23,84 @@ export default function Login({navigation}) {
   const [password, setPassword] = useState('');
   const [passError, setPassError] = useState(false);
 
-  const handleText = () => {
+  const passWrdRegz = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+  // function preg_match (regex, str) {
+  //   return (new RegExp(regex).test(str))
+  // }
+
+
+  function preg_match(regex, str) {
+    if (new RegExp(regex).test(str)) {
+      // return regex.exec(str);
+      return true
+    }
+    return false;
+  }
+
+  // const OnSubmit = (regex, str) => {
+  //   const isValid = preg_match(regex, str)
+  //   if (isValid) {
+  //     console.log('yes it matched')
+  //     alert(isValid)
+  //   }else{
+  //     alert('something went wrong')
+  //   }
+  // }
+
+
+  // function preg_match (emailRegex, passwordRegez, email, password) {
+  //   if (new RegExp(emailRegex).test(email) && RegExp(passwordRegez).test(password)){
+  //     // return regex.exec(str);
+  //     return true
+  //     // alert("evryThing matched ")
+  //   }
+  //   return false;
+  // }
+
+  const OnSubmit = (emailRegex, passWrdRegz, email, password) => {
+    
+    const isValid = preg_match(emailRegex, email,)
+    if (!isValid) {
+      alert('please input the correct email')
+    } else if (!preg_match(passWrdRegz, password,)) {
+      alert('please input the correct password')
+    } else {
+      navigation.navigate(navigationStrings.SIGNUP);
+    }
+
+  }
+
+
+
+  // const handleText = () => {
     // if (email != '') {
     //   setemailError(false);
 
-      if (emailRegex.test(email)) {
-        setemailError(false);
+  //   if (emailRegex.test(email)) {
+  //     setemailError(false);
 
-        if (password.length >= 8) {
-          setPassError(false);
-        // if(passRegex.test(password)){
-        //   setPassError(false)
-        
+  //     if (password.length >= 8) {
+  //       setPassError(false);
+  //       if (passRegex.test(password)) {
+  //         setPassError(false)
 
-          navigation.navigate(navigationStrings.HOME);
-        } else {
-          setPassError(true);
-        }
-      // } else {
-      //   setemailError(true);
-      }
-    // } else {
-    //   setemailError(true);
-    // }
-  };
+
+  //         navigation.navigate(navigationStrings.SIGNUP);
+  //       } else {
+  //         setPassError(true);
+  //       }
+  //       // } else {
+  //       //   setemailError(true);
+  //     }
+  //   } else {
+  //     setemailError(true);
+  //   }
+  // };
 
   return (
     <WrapperContainer>
-      <View style={{flex: 1, height: '100%', width: '100%', padding: 16}}>
+      <View style={{ flex: 1, height: '100%', width: '100%', padding: 16 }}>
         <View style={styles.loginview}>
           <Text style={styles.label1}>Welcome To</Text>
           <Text style={styles.label2}>E-Commerce</Text>
@@ -63,21 +113,21 @@ export default function Login({navigation}) {
             onChangeText={event => setemail(event)}
           />
         </View>
-        {emailerror?
-          <View style={{paddingBottom: 4}}>
-          <Text
-            style={{
-              textAlign: 'center',
-              color: 'red',
-              fontWeight: '300',
-              fontSize: 12,
-            }}>
-            Invalid Email
-          </Text>
-        </View>
-        :null
+        {emailerror ?
+          <View style={{ paddingBottom: 4 }}>
+            <Text
+              style={{
+                textAlign: 'center',
+                color: 'red',
+                fontWeight: '300',
+                fontSize: 12,
+              }}>
+              Invalid Email
+            </Text>
+          </View>
+          : null
         }
-        
+
         <View style={styles.passwordview}>
           <Image style={styles.passwordicon} source={imagePath.passwordIcon} />
           <TextInput
@@ -87,23 +137,23 @@ export default function Login({navigation}) {
             onChangeText={event => setPassword(event)}
           />
         </View>{
-          passError?
-           <View style={{paddingBottom: 4}}>
-           <Text
-             style={{
-               textAlign: 'center',
-               color: 'red',
-               fontWeight: '300',
-               fontSize: 12,
-             }}>
-             Invalid Password
-           </Text>
-         </View>:null
+          passError ?
+            <View style={{ paddingBottom: 4 }}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: 'red',
+                  fontWeight: '300',
+                  fontSize: 12,
+                }}>
+                Invalid Password
+              </Text>
+            </View> : null
         }
-       
+
 
         <View style={styles.btnview}>
-          <TouchableOpacity activeOpacity={0.3} onPress={handleText}>
+          <TouchableOpacity activeOpacity={0.3} onPress={() => { OnSubmit(emailRegex, passWrdRegz, email, password) }}>
             <Text style={styles.btn}> Sign In</Text>
           </TouchableOpacity>
         </View>
